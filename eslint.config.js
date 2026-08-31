a5d68070ebffe951afa9ba0b6a52f83922577ba4
Muhammad Royhan <muhroyhan.developer@gmail.com>
2026-08-31 17:13:13 +0700

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import { dirname } from "node:path"
+import { fileURLToPath } from "node:url"
+
+
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig([
+  {
+    ignores: ["docs/**", "**/dist/**"],
+  },
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.{ts,tsx}"],
+
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["*.ts"],
+        },
+        tsconfigRootDir: __dirname,
+      },
+    },
+
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+    },
+  },
+
+  eslintConfigPrettier,
+]);
